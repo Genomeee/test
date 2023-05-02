151f97b67f7d87d7412df8f093d42505d781238e
Misha Sydora <msydora@MacBook-Pro-Dream.local>
2023-05-02 05:02:43 +0300

    fix scripts

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS contact (
     matrix_id VARCHAR(200) NOT NULL,
     name VARCHAR(200) NOT NULL,
     tenant_id VARCHAR(200) NOT NULL,
-    PRIMARY KEY (tenant_id)
+    PRIMARY KEY (matrix_id)
 )
